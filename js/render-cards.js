458f44f39cbd2be7273d2e5edc3e6c550f1765f4
Daniil <daniel.vlasenko@gmail.com>
2024-03-29 20:28:08 +0300

    Добавляет функции по открытию и закрытию открытия миниатюры со всеми параметрами: фото, описание, кол-во лайков и комментариев, а также список комментариев

--- js/render-cards.js
+++ js/render-cards.js
@@ -9,14 +9,11 @@ const renderCards = (cardList) => {
     const pictureImage = picture.querySelector('.picture__img');
     const pictureComments = picture.querySelector('.picture__comments');
     const pictureLikes = picture.querySelector('.picture__likes');
-    picture.dataset.likes = card.likes;
 
     pictureImage.src = card.url;
     pictureImage.alt = card.description;
     pictureLikes.textContent = card.likes;
     pictureComments.textContent = card.comments.length;
-    picture.dataset.comments = card.comments.length;
-    picture.dataset.description = card.description;
     fragment.appendChild(picture);
   });
 
--- js/viewing-cards.js
+++ js/viewing-cards.js
@@ -1,77 +1,72 @@
-/*
-Написать программу, которая будет раскрывать миниатюры, в которых можно:
-1. Поставить лайк
-2. Увидеть все комментарии
-
-Миниатюра раскрывается по клику. Данные берутся из объекта для отрисовки.
-Чтобы раскрыть:
-1. + удалить класс .hidden у .big-picture и заполнить данными из миниатюры:
-  + а) src для .big-picture__img = url
-  + б) value для .likes-count = likes
-  + в) .social__comment-total-count = comments.length
-  г) .social__comments = comments
-  <li class="social__comment">
-    <img
-      class="social__picture"
-      src="{{аватар}}"
-      alt="{{имя комментатора}}"
-      width="35" height="35">
-    <p class="social__text">{{текст комментария}}</p>
-  </li>
-  д) .social__caption = description
-2. Добавить класс hidden и спрятать блоки счётчика комментариев .social__comment-count и и загрузки новых комментариев .comments-loader
-+ 3. Добавить body .modal-open для отмены скролла страницы
-
-При закрытии:
-1. Удалить .modal-open y body
-2. Прописать закрытие по ESC и клику по иконке
-*/
-
-// import { renderCards } from './render-cards';
-
-// const picturesContainer = document.querySelector('.pictures');
-// const picturesList = picturesContainer.children;
-
+import { isEscape } from './utils';
 
 const pictures = document.querySelector('.pictures');
-const closeButton = document.querySelector('.big-picture__cancel');
+const bigPicture = document.querySelector('.big-picture');
+const bigPictureClose = bigPicture.querySelector('.big-picture__cancel');
+const commentsList = bigPicture.querySelector('.social__comments');
+const commentTemplate = document.querySelector('#comment-item').content.querySelector('.social__comment');
 
-const closeBigCard = () => {
-  const bigPicture = document.querySelector('.big-picture');
-  bigPicture.classList.add('hidden');
-  document.body.classList.remove('modal-open');
+const fillBigPicture = (card) => {
+  const image = bigPicture.querySelector('.big-picture__img img');
+  image.src = card.url;
+  const likesCounter = bigPicture.querySelector('.likes-count');
+  likesCounter.textContent = card.likes;
+  const commentsCurrentCounter = bigPicture.querySelector('.social__comment-shown-count');
+  commentsCurrentCounter.textContent = card.comments.length;
+  const commentsCounter = bigPicture.querySelector('.social__comment-total-count');
+  commentsCounter.textContent = card.comments.length;
+
+  const ourFragment = document.createDocumentFragment();
+  card.comments.forEach((commentData) => {
+    const newCommentElement = commentTemplate.cloneNode(true);
+    const avatar = newCommentElement.querySelector('.social__picture');
+    avatar.src = commentData.avatar;
+    avatar.alt = commentData.name;
+    const text = newCommentElement.querySelector('.social__text');
+    text.textContent = commentData.message;
+    ourFragment.appendChild(newCommentElement);
+  });
+  commentsList.innerHTML = '';
+  commentsList.appendChild(ourFragment);
+
+  const description = document.querySelector('.social__caption');
+  description.textContent = card.description;
+};
+
+const onDocumentKeyDown = (evt) => {
+  if (isEscape(evt)) {
+    evt.preventDefault();
+    bigPicture.classList.add('hidden');
+    document.body.classList.remove('modal-open');
+  }
+};
+
+const openBigPicture = (evt, cardList) => {
+  if (evt.target.matches('.picture__img')) {
+    bigPicture.classList.remove('hidden');
+
+    document.body.classList.add('modal-open');
+
+    document.addEventListener('keydown', onDocumentKeyDown);
+
+    const pictureLink = evt.target.parentElement;
+    const id = Number(pictureLink.dataset.id);
+
+    const card = cardList.find((item) => item.id === id);
+
+    fillBigPicture(card);
+  }
 };
 
 const initialize = (cardList) => {
-
-  const getBigCard = (evt) => {
-    if (evt.target.matches('.picture__img')) {
-      const pictureLink = evt.target.parentElement;
-      const id = pictureLink.dataset.id;
-      const bigImage = document.querySelector('.big-picture__img img');
-      bigImage.src = evt.target.src;
-      const likesCounter = document.querySelector('.likes-count');
-      likesCounter.textContent = pictureLink.dataset.likes;
-      const commentsCounter = document.querySelector('.social__comment-total-count');
-      commentsCounter.textContent = pictureLink.dataset.comments;
-      const comments = document.querySelector('.social__comments');
-      // comments.appendChild(cardList[id].comments);
-      const description = document.querySelector('.social__caption');
-      description.textContent = pictureLink.dataset.description;
-
-      const bigPicture = document.querySelector('.big-picture');
-      bigPicture.classList.remove('hidden');
-
-
-      document.body.classList.add('modal-open');
-
-      closeButton.addEventListener('click', closeBigCard);
-    }
-  };
-
-
-  pictures.addEventListener('click', getBigCard);
-
+  pictures.addEventListener('click', (evt) => {
+    openBigPicture(evt, cardList);
+  });
+  bigPictureClose.addEventListener('click', () => {
+    bigPicture.classList.add('hidden');
+    document.body.classList.remove('modal-open');
+    document.removeEventListener('keydown', onDocumentKeyDown);
+  });
 };
 
 export {initialize};
